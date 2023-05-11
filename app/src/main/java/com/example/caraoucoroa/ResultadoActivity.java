283e039f8909f30f16f1609f7cfa8caf9d67bf5d
Vinicius Sato <vinicius.sato@outlook.com>
2023-05-10 23:42:52 -0300

    Adicionando funcionalidade do jogo

--- app/src/main/java/com/example/caraoucoroa/ResultadoActivity.java
+++ app/src/main/java/com/example/caraoucoroa/ResultadoActivity.java
@@ -3,12 +3,37 @@ package com.example.caraoucoroa;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class ResultadoActivity extends AppCompatActivity {
+
+    private ImageView imageResultado;
+    private ImageView imageVoltar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_resultado);
+
+        imageResultado = findViewById(R.id.imageResultado);
+        imageVoltar = findViewById(R.id.imageVoltar);
+
+        Bundle dados = getIntent().getExtras();
+        int numero = dados.getInt("numero");
+
+        if(numero == 0) {
+            imageResultado.setImageResource(R.drawable.moeda_cara);
+        }else {
+            imageResultado.setImageResource(R.drawable.moeda_coroa);
+        }
+
+        imageVoltar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+
     }
 }
